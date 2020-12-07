21067265e359be474fe3e6f044d66a3add8bf09a
a1pha1337 <45879854+a1pha1337@users.noreply.github.com>
2020-12-07 12:34:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -44,16 +44,16 @@ function main() {
 			scene.add(shop);
 		});
 	})
-	// var geometry = new THREE.BoxGeometry(2, 2, 2);
-	// var material = new THREE.MeshPhongMaterial({
-	// 	color: 0xffffff,
-	// 	side: THREE.DoubleSide
-	// });
+	var geometry = new THREE.BoxGeometry(0, 0, 0);
+	var material = new THREE.MeshPhongMaterial({
+		color: 0xffffff,
+		side: THREE.DoubleSide
+	});
 
-	// var cube = new THREE.Mesh(geometry, material);
-	// cube.receiveShadow = true;
-	// cube.position.set(32, 20, 13);
-	// scene.add(cube);
+	var cube = new THREE.Mesh(geometry, material);
+	cube.receiveShadow = true;
+	cube.position.set(32, 20, 13);
+	scene.add(cube);
 
 	// The first outside light
 	var pointLightIntensity = 1;
